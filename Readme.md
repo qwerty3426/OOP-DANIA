0995392191a02a0ca82f2efa69c2bce35c7b0a80
qwerty3426 <kotsiuba.a_ipz23@rcit.ukr.education>
2026-03-12 16:51:46 +0200

    Додав діаграму та оновив Readme

--- Readme.md
+++ Readme.md
@@ -1,2 +1,2 @@
 моя дівграма
-![alt text](image-1.png)
+![alt text](image.png)
--- obj/Debug/net9.0/OOP-DANIA.AssemblyInfo.cs
+++ obj/Debug/net9.0/OOP-DANIA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP-DANIA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ccb942bd8e3e86c807b4387186b3b702667453")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7f1c89832a92c6b5cbe322b4c350b1cc7cfaef")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP-DANIA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP-DANIA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
